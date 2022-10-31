6da10b2beb86586ef2278b62f6470397acb6737b
Ashton Yeackley <aey21@fsu.edu>
2022-10-31 13:29:18 -0400

    changed colors with key press

--- sketch.js
+++ sketch.js
@@ -1,17 +1,33 @@
 let paintSize = 25
+let color = '#000000'
 
 function setup() {
   createCanvas(displayWidth, displayHeight);
 }
 
 function draw() {
+  fill(color);
+  noStroke();
 }
 
-function mouseDragged() {  
-  let r = random(200,255);
-  let g = random(200,255);
-  let b = random(200,255);
-  fill(r,g,b);
-  noStroke();
+function mouseDragged() {
   ellipse(mouseX, mouseY, paintSize);
+}
+
+function keyTyped() {
+  if (key === 'p') {
+    color = '#d5d6ea';
+  } else if (key === 'y') {
+    color = '#f6f6eb';
+  } else if (key === 'g') {
+    color = '#d7ecdd';
+  } else if (key === 'o') {
+    color = '#f5d5cb';
+  } else if (key === 'b') {
+    color = '#cdecff';
+  } else if (key === 'w') {
+    color = '#ffffff';
+  } else if (key === 'x') {
+    color = '#000000';
+  } 
 }
