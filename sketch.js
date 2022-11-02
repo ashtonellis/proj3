c4b6b3346bb7a8150417d679ef6f453123097b84
Ashton Yeackley <aey21@fsu.edu>
2022-11-02 13:14:05 -0400

    removed control image, added control buttons

--- sketch.js
+++ sketch.js
@@ -1,10 +1,7 @@
-let paintSize = 25
-let color = '#000000'
-let img;
-
-function preload() {
-  img = loadImage('assets/controls.png')
-}
+let paintSize = 25;
+let color = '#000000';
+let cir = true;
+let sq = false;
 
 function setup() {
   createCanvas(displayWidth, displayHeight);
@@ -13,41 +10,83 @@ function setup() {
 function draw() {
   fill(color);
   noStroke();
-  image(img, 0, 0, 300,120);
+  pur = createButton('purple');
+    pur.style('background-color', '#dabfff')
+    pur.position(0, 0);
+    pur.mousePressed(colorP);
+  bl = createButton('blue');
+    bl.style('background-color', '#aaefdf')
+    bl.position(51, 0);
+    bl.mousePressed(colorB);
+  gr = createButton('green');
+    gr.style('background-color', '#c3e991')
+    gr.position(90, 0);
+    gr.mousePressed(colorG);
+  ye = createButton('yellow');
+    ye.style('background-color', '#fde74c')
+    ye.position(138, 0);
+    ye.mousePressed(colorY);
+  or = createButton('orange');
+    or.style('background-color', '#f18f01')
+    or.position(189, 0);
+    or.mousePressed(colorO);
+  ba = createButton('black');
+    ba.style('background-color', '#000000');
+    ba.style('color', '#ffffff');
+    ba.position(244, 0);
+    ba.mousePressed(colorBa);
+  wh = createButton('white');
+    wh.style('background-color', '#ffffff');
+    wh.position(289, 0);
+    wh.mousePressed(colorW);
+  sm = createButton('smaller');
+    sm.style('background-color','#ffffff');
+    sm.position(0,25);
+    sm.mousePressed(sizeSm);
+  lg = createButton('larger');
+    lg.style('background-color','#ffffff');
+    lg.position(56,25);
+    lg.mousePressed(sizeLg);
+  cr = createButton('circle');
+    cr.style('background-color','#ffffff');
+    cr.position(104,25);
+    cr.mousePressed(brushC);
+  sq = createButton('square');
+    sq.style('background-color','#ffffff');
+    sq.position(149,25);
+    sq.mousePressed(brushS);
+  bkb = createButton ('background-black');
+    bkb.style('background-color', '#000000');
+    bkb.style('color', '#ffffff');
+    bkb.position(204,25);
+    bkb.mousePressed(backB);
+  bwb = createButton ('background-white');
+    bwb.style('background-color','#ffffff');
+    bwb.position(323,25);
+    bwb.mousePressed(backW);
 }
+
+function colorP() {color = '#dabfff';}
+function colorB() {color = '#aaefdf';}
+function colorG() {color = '#c3e991';}
+function colorY() {color = '#fde74c';}
+function colorO() {color = '#f18f01';}
+function colorBa() {color = '#000000';}
+function colorW() {color = '#ffffff';}
+
+function sizeSm() {paintSize = paintSize -= 5;}
+function sizeLg() {paintSize = paintSize += 5;}
+
+function brushC() {cir = true; sq = false;}
+function brushS() {cir = false; sq = true;}
+
+function backB() {background(0);}
+function backW() {background(255);}
 
 function mouseDragged() {
-  ellipse(mouseX, mouseY, paintSize);
-}
-
-function keyTyped() {
-  if (key === 'p') {
-    color = '#dabfff';
-  } else if (key === 'y') {
-    color = '#fde74c';
-  } else if (key === 'g') {
-    color = '#c3e991';
-  } else if (key === 'o') {
-    color = '#f18f01';
-  } else if (key === 'b') {
-    color = '#aaefdf';
-  } else if (key === 'w') {
-    color = '#ffffff';
-  } else if (key === 'x') {
-    color = '#000000';
-  } 
-}
-
-function keyPressed() {
-  if (keyCode === UP_ARROW) {
-    paintSize = paintSize += 5;
-  } else { if (keyCode === DOWN_ARROW) {
-    paintSize = paintSize -= 5;
+  if (cir) {
+    ellipse(mouseX, mouseY, paintSize);
+  } else { if (sq) {
+    rect(mouseX,mouseY,paintSize);
   } }
-
-  if (keyCode === CONTROL) {
-    background(0);
-  } else { if (keyCode === ALT) {
-    background(255);
-  } } 
 }
